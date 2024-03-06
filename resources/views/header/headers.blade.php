21dfd86279913eac8a848f19620854835cff9d61
jojotrance <139723099+jojotrance@users.noreply.github.com>
2024-03-06 16:12:00 +0800

    Update headers.blade.php

--- resources/views/header/headers.blade.php
+++ resources/views/header/headers.blade.php
@@ -1,4 +1,4 @@
-<header id="header">
+<!-- <header id="header">
     <nav class="navbar navbar-expand-lg navbar-white" style="background-color: #333d29;">
         <a class="navbar-brand" href="{{ route('home') }}" style="color: #343a40;">
             <h3 class="px-5">
@@ -29,6 +29,30 @@
                 </a>
                 <a class="nav-link" href="{{ route('parking.index') }}" style="color: #343a40;">
                     <i class="fas fa-car"></i> Parking
+                </a>
+            </div>
+        </div>
+    </nav>
+</header> -->
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-white" style="background-color: #333d29;">
+        <a class="navbar-brand" href="{{ route('user.index') }}" style="color: #343a40;">
+            <h3 class="px-5">
+                <i class="fas fa-building"></i>BeyondTheStalls
+            </h3>
+        </a>
+        <button class="navbar-toggler" type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+                <a class="nav-link" href="{{ route('tenant.stall') }}" style="color: #343a40;">
+                    <i class="fas fa-warehouse"></i> My Stall
                 </a>
             </div>
         </div>
