379e2417ec84e669835758dc518e8e0efcc5c11b
jojotrance <139723099+jojotrance@users.noreply.github.com>
2024-03-06 15:34:19 +0800

    Update analytics.blade.php

--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -5,13 +5,38 @@
         <x-slot name="header">
             @include('header.headers')
         </x-slot>
+<style>
+    
+.card-header-colored {
+  background-color: #3a5a40; 
+  color: white; 
+  padding: 10px;
+}
+body {
+        background-image: url('/images/430837379_333900309104849_4293786303333364672_n.png');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
+      }
 
+    img {
+        max-width: 100%;
+        height: 100%;
+        height:auto;
+        background:#e0e2e4 ;
+        background: radial-gradient(white 30%, #e0e2e4 70%);
+
+    }
+
+</style>
         <div class="container">
             <div class="row">
                 <!-- Tenant Demographics by Address Chart -->
                 <div class="col-md-6">
                     <div class="card">
-                        <div class="card-header">Tenant Demographics by Address</div>
+                        <div class="card-header card-header-colored">Tenant Demographics by Address</div>
                         <div class="card-body">
                             {!! $tenantByAddressChart->container() !!}
                         </div>
@@ -21,7 +46,7 @@
                 <!-- Stall Statuses Chart -->
                 <div class="col-md-6">
                     <div class="card">
-                        <div class="card-header">Stall Statuses</div>
+                        <div class="card-header card-header-colored">Stall Statuses</div>
                         <div class="card-body">
                             {!! $stallStatusesChart->container() !!}
                         </div>
@@ -33,7 +58,7 @@
                 <!-- User vs Tenant Count Chart -->
                 <div class="col-md-6">
                     <div class="card">
-                        <div class="card-header">User vs Tenant Count</div>
+                        <div class="card-header card-header-colored">User vs Tenant Count</div>
                         <div class="card-body">
                             {!! $countChart->container() !!}
                         </div>
@@ -43,7 +68,7 @@
                 <!-- Parking Charges by Day Chart -->
                 <div class="col-md-6">
                     <div class="card">
-                        <div class="card-header">Parking Charges by Day</div>
+                        <div class="card-header card-header-colored">Parking Charges by Day</div>
                         <div class="card-body">
                             {!! $parkingChargesChart->container() !!}
                         </div>
